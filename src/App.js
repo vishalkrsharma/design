2b09e6367e5b6210f339752d6e17fa4fa11da91f
vishalkrsharma <vishalkrsharma1234@gmail.com>
2023-01-10 23:40:19 +0530

    done

--- src/App.js
+++ src/App.js
@@ -49,29 +49,32 @@ function App() {
     <div className='h-max'>
       <div className='container mt-5 mx-auto' style={{ maxWidth: '800px' }}>
         <div className='heading p-5 pb-0'>
-          <h1 className='text-2xl font-medium mb-1'>Challenge</h1>
-          <h3 className='text-sm text-slate-600'>Complete your daily tasks to fast track your preparation.</h3>
+          <div className='text-2xl font-medium mb-1'>Challenge</div>
+          <div className='text-sm text-slate-600'>Complete your daily tasks to fast track your preparation.</div>
         </div>
 
-        <div className='container p-5'>
-          <div className='main p-5 lg:flex justify-between items-center'>
+        <div className='container p-5 mx-auto'>
+          <div className='main p-5 md:flex justify-between items-center'>
             <div className='container flex items-center'>
               <div className='rounded-full bg-sky-100 inline-block mr-3'>
                 <img src={runner} alt={runner} />
               </div>
-              <div>
+              <div className='flex-1'>
                 <h1 className='header-main text-lg'>Your Doing Great! Keep Going</h1>
                 <div className='header-sub text-base text-slate-600'>You Are in Second in the Group</div>
               </div>
             </div>
-            <div className='container flex items-center max-lg:mt-5 max-lg:ml-16 '>
+            <div className='container flex items-center md:justify-end justify-start max-lg:mt-5 '>
               <img className='h-5 mr-2' src={star} alt={star} />
               <h2>You Just Need 3 Points to Get First</h2>
             </div>
           </div>
           <div className='body'>
             {peopleArray.map((people, idx) => (
-              <People people={people} key={idx} />
+              <>
+                <People people={people} key={idx} />
+                <div className='divider w-full' style={{ height: '2px', backgroundColor: '#f4f4f4' }}></div>
+              </>
             ))}
           </div>
         </div>
--- src/components/People.js
+++ src/components/People.js
@@ -8,25 +8,22 @@ import point from '../assets/Vector-3.png';
 
 function People({ people }) {
   const stars = [star0, star1, star2, star3];
-  const aspectRatio = [26 / 25, 26 / 25, 45 / 27, 64 / 27];
+
   return (
-    <div className='people p-2 flex justify-between'>
-      <img
-        className='w-20'
-        // style={{ aspectRatio: `${aspectRatio[people.stars]}` }}
-        src={stars[people.stars]}
-        alt={stars[people.stars]}
-      />
-      <div className='user-info flex items-center flex-1'>
+    <div className='people py-2 px-5 flex relative'>
+      <div style={{ width: '47.4px' }}>
+        <img className='h-5 mt-6 mx-auto' src={stars[people.stars]} alt={stars[people.stars]} />
+      </div>
+      <div className='user-info flex items-center basis-3/5 ml-3'>
         <img className='h-12 m-2' src={people.avatar} alt={people.avatar} />
         <div className='name'>
           <h2>{people.name}</h2>
           <h2 className='text-slate-600'>{people.sub}</h2>
         </div>
       </div>
-      <div className='score-cont flex items-center flex-1 relative'>
-        <img className='absolute top-3 w-5' src={point} alt={point} />
-        <div className='score absolute left-8'>
+      <div className='score-cont flex items-center'>
+        <img className='-mt-5' src={point} alt={point} />
+        <div className='score ml-2'>
           <div className='head text-xl font-medium'>{people.points}</div>
           <div className='sub text-slate-600'>Reward Points</div>
         </div>
